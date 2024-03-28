c520cfe3bb7d05edaaf6ecc6f94b565c4193646a
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-03-28 13:18:31 +0900

    feat: Add vite-tsconfig-paths.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vitest/config";
 import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   build: {
@@ -12,7 +13,7 @@ export default defineConfig({
       fileName: "index",
     },
   },
-  plugins: [dts({ exclude: "src/example/" })],
+  plugins: [dts({ exclude: "src/example/" }), tsconfigPaths()],
   test: {
     globals: true,
     setupFiles: ["./setup.ts"],
